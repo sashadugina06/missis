9529fd91704e58cc4ac31a75051ce504e106e0de
sashadugina06 <sd27012006@gmail.com>
2022-03-25 17:58:42 +0300

    lesson 4

--- project/script.js
+++ project/script.js
@@ -1,6 +1,6 @@
 //alert("hgmntr");
 
-//alert(a);
+
 
 
 function guess() {
@@ -16,4 +16,35 @@ function guess() {
     }
 }
 
-guess();
+//guess();
+
+function timer() {
+    alert("Ты тут долго");
+}
+
+//setTimeout(timer, 3000);
+
+function valid() {
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+    var tel = document.getElementById("tel").value;
+
+    var reg_name = /^[а-яa-zё]+$/i;
+    var reg_password = /^[a-z0-9]{5,100}/i;
+    var reg_tel = /^[0-9]{11}/i;
+
+    if (reg_name.test(name) == false) {
+        alert("ошибка имя");
+    }
+    if (reg_password.test(password) == false) {
+        alert("ошибка пароль");
+
+    }
+    if (reg_tel.test(tel) == false) {
+        alert("ошибка телефон");
+    }
+
+
+}
+
+document.querySelector(".button").addEventListener("click", valid);
